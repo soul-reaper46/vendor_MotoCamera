2b4938da76282a132ed8c5fe3982eed9350893e1
althafvly <althafvly@gmail.com>
2020-04-22 17:01:44 +0530

    Update copies flags

--- config.mk
+++ config.mk
@@ -15,10 +15,17 @@
 VENDOR_PATH := vendor/MotoCamera
 
 PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc/permissions,$(TARGET_COPY_OUT_PRODUCT)/etc/permissions) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc/sysconfig,$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig) \
 	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/framework,$(TARGET_COPY_OUT_PRODUCT)/framework) \
    	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MotCamera/lib/arm,$(TARGET_COPY_OUT_PRODUCT)/priv-app/MotCamera/lib/arm)
+
+# Motorola Camera permissions
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/system/etc/permissions/com.motorola.androidx.camera.extensions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.androidx.camera.extensions.xml \
+    $(VENDOR_PATH)/system/etc/permissions/com.motorola.cameraone.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.cameraone.xml \
+    $(VENDOR_PATH)/system/etc/permissions/deviceowner-configuration-com.motorola.cameraone.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/deviceowner-configuration-com.motorola.cameraone.xml \
+    $(VENDOR_PATH)/system/etc/permissions/privapp-permissions-com.motorola.cameraone.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-com.motorola.cameraone.xml \
+    $(VENDOR_PATH)/system/etc/permissions/com.motorola.motosignature.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.motosignature.xml \
+    $(VENDOR_PATH)/system/etc/sysconfig/hiddenapi-whitelist-com.motorola.cameraone.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hiddenapi-whitelist-com.motorola.cameraone.xml
 
 PRODUCT_PACKAGES += \
     MotCamera \
